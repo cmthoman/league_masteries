810dc42e6b0f895d2f012b0f0fa528cfa3c936bc
Chris <epimyth@gmail.com>
2014-01-06 09:06:50 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeagueMasteries::Application.config.secret_key_base = 'f151492e80ad2c79551cdfc69e25a1f0393e94a3dc246746a9696116f442687019baaed504655e7b59b8970a9d79556341c0c76b515ed108068bc57d8fa1ffdc'
