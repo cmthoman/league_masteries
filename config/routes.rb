ae180cbcd7a4ad8011fea4921f0a75378bb16706
Chris <epimyth@gmail.com>
2014-01-06 10:21:41 -0500

    added basic layout rows and masteries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LeagueMasteries::Application.routes.draw do
+
+  root to: 'masteries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
